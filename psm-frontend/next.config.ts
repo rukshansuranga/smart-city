b9952b23e029358e30130f21a2bcecbfe0a89710
rukshan <rukshansuranga@gmail.com>
2025-07-21 13:31:37 +0530

    add cmd to action

--- psm-frontend/next.config.ts
+++ psm-frontend/next.config.ts
@@ -3,7 +3,7 @@ import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: "app/build",
+  distDir: "build",
 };
 
 export default withFlowbiteReact(nextConfig);
