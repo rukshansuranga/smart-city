695a169827e83fed4a7fd29466dae855767d6150
rukshansuranga <rukshansuranga@gmail.com>
2025-10-30 19:03:05 +0530

    Delete psm-mobile directory

--- psm-mobile/api/clientAction.ts
+++
@@ -1,7 +0,0 @@
-import { fetchWrapper } from "@/lib/fetchWrapper";
-import { ApiResponse, Client } from "@/types";
-
-export async function postClient(client: Client): Promise<ApiResponse<Client>> {
-  console.log("Posting client:", client);
-  return fetchWrapper.post("client", client);
-}
--- psm-mobile/complainMaps.js
+++
@@ -1,5 +0,0 @@
-export const complainMap = [
-  { id: "lightisnotworking", name: "Light is not working" },
-  { id: "switchisnotworking", name: "Switch is not working" },
-  { id: "postisdamaged", name: "Post is damaged" },
-];
